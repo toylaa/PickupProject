d0358b5e08d1663a70ad0989633cf540c77ac885
Tyler McDonough <tcmcd1337@gmail.com>
2017-12-18 21:14:43 +0000

    migration

--- CurrentBuild/connect.php
+++ CurrentBuild/connect.php
@@ -1,7 +1,13 @@
 <?php
-    // A simple PHP script demonstrating how to connect to MySQL.
-    // Press the 'Run' button on the top to start the web server,
-    // then click the URL that is emitted to the Output tab of the console.
+   //https://community.c9.io/t/setting-up-mysql/1718
+    /*
+    Hostname - $IP (The same local IP as the application you run on Cloud9)
+    Port - 3306 (The default MySQL port number)
+    User - $C9_USER (Your Cloud9 user name)
+    Password - "" (No password since you can only access the DB from within the workspace)
+    Database - c9 (The database username)
+    */
+     //>> mysql-ctl cli 
 
     $servername = getenv('IP');
     $username = getenv('C9_USER');
@@ -11,5 +17,7 @@
 
     // Create connection
     $db = new mysqli($servername, $username, $password, $database, $dbport);
+
+   
 
 ?>
